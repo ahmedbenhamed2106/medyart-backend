1e476b7d39adeb2f9773d455c8306e5d42b83ef3
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-01 14:29:46 +0100

    Enhance manage.py with BASE_DIR and error handling
    Updated the manage.py file to include BASE_DIR for path resolution and improved the error message for Django import.

--- medyart-backend/manage.py
+++ medyart-backend/manage.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 import os
 import sys
+from pathlib import Path
 
 def main():
+    # Force Python to find the 'medyart' directory from the root
+    BASE_DIR = Path(__file__).resolve().parent
+    sys.path.insert(0, str(BASE_DIR))
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medyart.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed?"
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
