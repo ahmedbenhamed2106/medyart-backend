77a50571d78b399c97f01ef081308e2c859eb7fe
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-01 17:06:07 +0100

    Update ROOT_URLCONF to 'medyart.urls'

--- medyart-backend/medyart/settings.py
+++ medyart-backend/medyart/settings.py
@@ -29,7 +29,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'medyart.urls'
 
 DATABASES = {
     'default': dj_database_url.config(
