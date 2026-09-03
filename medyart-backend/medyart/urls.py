b167941443b64c69b1422a287266c46d49ad1a8a
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-03 15:58:06 +0100

    Add PhotoViewSet to API routes

--- medyart-backend/medyart/urls.py
+++ medyart-backend/medyart/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from gallery.views import PhotoViewSet
+
+router = DefaultRouter()
+router.register(r'photos', PhotoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
