428889820019db3296b8cee8f67b6fb083c6a6ba
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-03 15:50:38 +0100

    Add REST framework and CORS middleware to settings
    Added Django REST framework and CORS headers to settings.

--- medyart-backend/medyart/settings.py
+++ medyart-backend/medyart/settings.py
@@ -17,9 +17,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gallery',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,3 +59,6 @@ DATABASES = {
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
