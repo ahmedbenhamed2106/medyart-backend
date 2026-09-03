2e945df8477be2a011c909988549ac66397b5582
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-03 15:56:42 +0100

    Add PhotoSerializer for PhotoModel

---
+++ medyart-backend/gallery/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import PhotoModel
+
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhotoModel
+        fields = ['id', 'title', 'image_url', 'created_at']
