361d98d654504d548c0b96b4f3f377f6cc586a05
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-03 16:33:24 +0100

    Add Comment and Interaction serializers

--- medyart-backend/gallery/serializers.py
+++ medyart-backend/gallery/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
-from gallery.models import PhotoModel
+from django.contrib.auth.models import User
+from gallery.models import PhotoModel, InteractionModel, CommentModel
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = CommentModel
+        fields = ['id', 'user', 'photo', 'text', 'created_at']
+
+class InteractionSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = InteractionModel
+        fields = ['id', 'user', 'photo', 'vote', 'created_at']
 
 class PhotoSerializer(serializers.ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+    dislikes_count = serializers.SerializerMethodField()
+    comments = CommentSerializer(many=True, read_only=True)
+
     class Meta:
         model = PhotoModel
-        fields = ['id', 'title', 'image_url', 'created_at']
+        fields = ['id', 'title', 'image_url', 'created_at', 'likes_count', 'dislikes_count', 'comments']
+
+    def get_likes_count(self, obj):
+        return obj.interactions.filter(vote='like').count()
+
+    def get_dislikes_count(self, obj):
+        return obj.interactions.filter(vote='dislike').count()
