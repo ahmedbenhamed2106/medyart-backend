f046ca3f6f5b6560078d93adbe3038d4c0977319
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-03 16:55:56 +0100

    Add Comment, Interaction, and Register routes

--- medyart-backend/medyart/urls.py
+++ medyart-backend/medyart/urls.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from gallery.views import PhotoViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from gallery.views import (
+    PhotoViewSet,
+    CommentViewSet,
+    InteractionViewSet,
+    RegisterView
+)
 
 router = DefaultRouter()
 router.register(r'photos', PhotoViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'interactions', InteractionViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/register/', RegisterView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
