738cce56cb9989590d8882d217b8809a7bf0b574
ahmedbenhamed2106 <ahmed.benhamed.2106@gmail.com>
2026-09-12 08:24:59 +0100

    Add EmailOrUsernameModelBackend for authentication
    Implement custom authentication backend to allow login with email or username.

---
+++ medyart-backend/gallery/backends.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+from django.db.models import Q
+
+class EmailOrUsernameModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get('email')
+        try:
+            user = User.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+        except User.DoesNotExist:
+            return None
+        except User.MultipleObjectsReturned:
+            user = User.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).first()
+
+        if user and user.check_password(password):
+            return user
+        return None
